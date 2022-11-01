040c14d265957a6be594ba168472640a1b351c84
Angelo Barandino <angelobarandino24@gmail.com>
2022-11-01 22:36:53 +0800

    add success decorator

---
+++ Runtime/Decorators/ReturnSuccess.cs
@@ -0,0 +1,15 @@
+﻿namespace BehaviorGraph.Runtime.Tasks.Decorators
+{
+    public class ReturnSuccess : Decorator
+    {
+        protected override NodeState OnUpdate()
+        {
+            if (child.Evaluate() == NodeState.Running)
+            {
+                return NodeState.Running;
+            }
+
+            return NodeState.Success;
+        }
+    }
+}
